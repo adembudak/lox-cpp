9220657f388aefb7804f09e344bb60c399f72395
vanilla milkshake <vanillamilkshakepls@gmail.com>
2022-01-30 23:02:49 +0300

    Implemented expression nodes

--- include/lox/ast/ast.h
+++ include/lox/ast/ast.h
@@ -2,8 +2,125 @@
 
 #include "lox/token.h"
 
-#include <memory>
-#include <variant>
-#include <vector>
+#include <boost/variant/variant.hpp>
+#include <boost/variant/recursive_wrapper.hpp>
 
-namespace lox {}
+// https://craftinginterpreters.com/appendix-i.html
+
+namespace lox {
+
+struct AssignExpr;
+struct BinaryExpr;
+struct CallExpr;
+struct GetExpr;
+struct GroupingExpr;
+struct LiteralExpr;
+struct LogicalExpr;
+struct SetExpr;
+struct SuperExpr;
+struct ThisExpr;
+struct UnaryExpr;
+struct VariableExpr;
+
+using boost::recursive_wrapper;
+using boost::variant;
+
+// clang-format off
+using Expr = variant<recursive_wrapper<AssignExpr>,   
+                     recursive_wrapper<BinaryExpr>, 
+                     recursive_wrapper<CallExpr>,     
+                     recursive_wrapper<GetExpr>,
+                     recursive_wrapper<GroupingExpr>, 
+                     recursive_wrapper<LiteralExpr>, 
+                     recursive_wrapper<LogicalExpr>,  
+                     recursive_wrapper<SetExpr>,
+                     recursive_wrapper<SuperExpr>,    
+                     recursive_wrapper<ThisExpr>, 
+                     recursive_wrapper<UnaryExpr>,    
+                     recursive_wrapper<VariableExpr>>;
+// clang-format on
+struct AssignExpr {
+  Token name;
+  Expr value;
+
+  AssignExpr(Token &name, Expr &val);
+};
+
+struct BinaryExpr {
+  Expr left;
+  Token op;
+  Expr right;
+
+  BinaryExpr(Expr &left, Token &op, Expr &right);
+};
+
+struct CallExpr {
+  Expr callee;
+  Token paren;
+  std::vector<Expr> arguments;
+
+  CallExpr(Expr &callee, Token &paren, std::vector<Expr> &arguments);
+};
+
+struct GetExpr {
+  Expr object;
+  Token name;
+
+  GetExpr(Expr &object, Token &name);
+};
+
+struct GroupingExpr {
+  Expr expression;
+
+  GroupingExpr(Expr &expression);
+};
+
+struct LiteralExpr {
+  literal_t literal;
+
+  LiteralExpr(literal_t &literal);
+};
+
+struct LogicalExpr {
+  Expr left;
+  Token op;
+  Expr right;
+
+  LogicalExpr(Expr &left, Token &op, Expr &right);
+};
+
+struct SetExpr {
+  Expr obj;
+  Token name;
+  Expr val;
+
+  SetExpr(Expr &obj, Token &name, Expr &val);
+};
+
+struct SuperExpr {
+  Token keyword;
+  Token method;
+
+  SuperExpr(Token &keyword, Token &method);
+};
+
+struct ThisExpr {
+  Token keyword;
+
+  ThisExpr(Token &keyword);
+};
+
+struct UnaryExpr {
+  Token op;
+  Expr right;
+
+  UnaryExpr(Token &op, Expr &right);
+};
+
+struct VariableExpr {
+  Token name;
+
+  VariableExpr(Token &name);
+};
+
+}
--- src/ast/ast.cpp
+++ src/ast/ast.cpp
@@ -4,3 +4,63 @@
 #include <vector>
 
 using namespace lox;
+
+AssignExpr::AssignExpr(Token &name, Expr &val)
+    : name(name)
+    , value(val) {
+}
+
+BinaryExpr::BinaryExpr(Expr &left, Token &op, Expr &right)
+    : left(left)
+    , op(op)
+    , right(right) {
+}
+
+CallExpr::CallExpr(Expr &callee, Token &paren, std::vector<Expr> &arguments)
+    : callee(callee)
+    , paren(paren)
+    , arguments(arguments) {
+}
+
+GetExpr::GetExpr(Expr &object, Token &name)
+    : object(object)
+    , name(name) {
+}
+
+GroupingExpr::GroupingExpr(Expr &expression)
+    : expression(expression) {
+}
+
+LiteralExpr::LiteralExpr(literal_t &literal)
+    : literal(literal) {
+}
+
+LogicalExpr::LogicalExpr(Expr &left, Token &op, Expr &right)
+    : left(left)
+    , op(op)
+    , right(right) {
+}
+
+SetExpr::SetExpr(Expr &obj, Token &name, Expr &val)
+    : obj(obj)
+    , name(name)
+    , val(val) {
+}
+
+SuperExpr::SuperExpr(Token &keyword, Token &method)
+    : keyword(keyword)
+    , method(method) {
+}
+
+ThisExpr::ThisExpr(Token &keyword)
+    : keyword(keyword) {
+}
+
+UnaryExpr::UnaryExpr(Token &op, Expr &right)
+    : op(op)
+    , right(right) {
+}
+
+VariableExpr::VariableExpr(Token &name)
+    : name(name) {
+}
