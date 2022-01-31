b54b30509675f1e2d5f73d60c1a9e9e30ccaceae
vanilla milkshake <vanillamilkshakepls@gmail.com>
2022-01-31 04:11:18 +0300

    Implemented: helpers for the parser

--- include/lox/parser/parser.h
+++ include/lox/parser/parser.h
@@ -1,11 +1,29 @@
 #pragma once
 
 #include "lox/token.h"
-#include "lox/ast/ast.h"
 
 #include <vector>
-#include <initializer_list>
 #include <cstdint>
-#include <string_view>
 
-namespace lox {}
+namespace lox {
+
+class Parser {
+private:
+  std::vector<Token> m_tokens;
+  std::size_t m_current = 0;
+
+public:
+  Parser(const std::vector<Token> &tokens)
+      : m_tokens(tokens) {
+  }
+
+private:
+  Token peek() const;
+  bool isAtEnd() const;
+  Token previous() const;
+  bool check(const tokenKind t) const;
+  Token advance();
+  bool match(std::initializer_list<tokenKind> tokens);
+};
+
+}
--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -1,8 +1,41 @@
-#include "lox/token.h"
 #include "lox/parser/parser.h"
+#include "lox/token.h"
 
-#include <cassert>
-#include <string_view>
-#include <vector>
+#include <initializer_list>
+#include <algorithm>
 
 using namespace lox;
+
+Token Parser::peek() const {
+  return m_tokens[m_current];
+}
+
+bool Parser::isAtEnd() const {
+  return peek().m_kind == tokenKind::END_OF_FILE;
+}
+
+Token Parser::previous() const {
+  return m_tokens[m_current - 1];
+}
+
+Token Parser::advance() {
+  if (!isAtEnd()) {
+    ++m_current;
+  }
+  return previous();
+}
+
+bool Parser::check(const tokenKind t) const {
+  if (isAtEnd())
+    return false;
+  return peek().m_kind == t;
+}
+
+bool Parser::match(std::initializer_list<tokenKind> tokens) {
+  bool isThereAnMatch = std::any_of(tokens.begin(), tokens.end(), [&](const auto token) { return check(token); });
+
+  if (isThereAnMatch) {
+    advance();
+  }
+  return isThereAnMatch;
+}
