6a4bd9d0f55968d95d6e81ec7f409c6f674f51cb
vanilla milkshake <vanillamilkshakepls@gmail.com>
2022-02-26 22:44:20 +0300

    Added test main
    The implementation can run `hello world` program now:
    
    $ echo "print \"hello world\";" > hello-world.lox
    $ ./build/cli/lox-cli hello-world.lox
    hello world

---
+++ cli/main.cpp
@@ -0,0 +1,60 @@
+#include <lox/interpreter/interpreter.h>
+#include <lox/scanner/scanner.h>
+#include <lox/parser/parser.h>
+#include <lox/prettyprint/prettyprint.h>
+
+#include <string>
+#include <iterator>
+#include <fstream>
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+namespace {
+
+void run(const std::string &source) {
+  lox::Scanner scanner(source);
+  std::vector tokens = scanner.scan();
+
+  lox::Parser parser(tokens);
+  std::vector statements = parser.parse();
+
+  lox::Interpreter interpreter(statements);
+  interpreter.interpret();
+}
+
+void runFile(const std::string &sourceFile) {
+  std::ifstream fin;
+  fin.open(sourceFile);
+  // assert(fin.is_open());
+
+  const auto source = std::string(std::istream_iterator<char>(fin >> std::noskipws), {});
+
+  run(source);
+}
+
+void runPrompt() {
+  for (;;) {
+    std::cout << "> ";
+    std::string input;
+    std::getline(std::cin, input);
+    if (!input.empty()) {
+      run(input);
+    }
+  }
+}
+
+}
+
+int main(int argc, const char *const argv[]) {
+  if (argc < 2) {
+    std::cout << "Usage: jlox [script]\n";
+    return 1;
+  } else if (argc == 2) { // execute file
+    runFile(argv[1]);
+  } else { // REPL
+    runPrompt();
+  }
+
+  return 0;
+}
