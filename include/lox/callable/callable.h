e45a834e0f18eb8b7dbf5b710755f3068279f5ea
vanilla milkshake <vanillamilkshakepls@gmail.com>
2022-04-11 20:13:26 +0300

    Move callable implementation to callable.cpp

--- include/lox/callable/callable.h
+++ include/lox/callable/callable.h
@@ -1,19 +1,16 @@
 #pragma once
 
-#include <variant>
 #include "lox/callable/class/class.h"
 #include "lox/callable/function/function.h"
-#include "lox/environment/environment.h"
-#include "lox/interpreter/interpreter.h"
+
+#include <variant>
+#include <cstdint>
+#include <string>
 
 namespace lox {
-template <class... Ts>
-struct overload : Ts... {
-  using Ts::operator()...;
-};
 
-template <class... Ts>
-overload(Ts...) -> overload<Ts...>;
+class Class;
+class Function;
 
 class Callable {
 public:
@@ -24,41 +21,12 @@ private:
 
 public:
   Callable() = default;
+  Callable(const Class &klass);
+  Callable(const Function &function);
 
-  Callable(const Function &function)
-      : m_callable(function) {
-  }
-
-  Callable(const Class &klass)
-      : m_callable(klass) {
-  }
-
-  std::any call(const Interpreter &interpreter, const std::vector<Literal> &arguments) const {
-    return std::visit(overload{
-                          [&](const std::monostate) { return std::any{}; },                        //
-                          [&](const Class &klass) { return klass.call(interpreter, arguments); },         //
-                          [&](const Function &function) { return function.call(interpreter, arguments); } //
-                      },
-                      m_callable);
-  }
-
-  std::size_t arity() const {
-    return std::visit(overload{
-                          [&](const std::monostate) { return std::size_t{0}; },      //
-                          [&](const Class &klass) { return klass.arity(); },         //
-                          [&](const Function &function) { return function.arity(); } //
-                      },
-                      m_callable);
-  }
-
-  operator std::string() const {
-    return std::visit(overload{
-                          [&](const std::monostate) { return std::string{}; },            //
-                          [&](const Class &klass) { return std::string(klass); },         //
-                          [&](const Function &function) { return std::string(function); } //
-                      },
-                      m_callable);
-  }
+  std::any call(const Interpreter &interpreter, const std::vector<Literal> &arguments) const;
+  std::size_t arity() const;
+  operator std::string() const;
 };
 
 }
---
+++ src/callable/callable.cpp
@@ -0,0 +1,58 @@
+#include "lox/callable/callable.h"
+#include "lox/environment/environment.h"
+#include "lox/interpreter/interpreter.h"
+
+#include <variant>
+#include <cstdint>
+#include <string>
+
+namespace {
+
+template <class... Ts>
+struct overload : Ts... {
+  using Ts::operator()...;
+};
+
+template <class... Ts>
+overload(Ts...) -> overload<Ts...>;
+
+}
+
+namespace lox {
+
+Callable::Callable(const Class &klass)
+    : m_callable(klass) {
+}
+
+Callable::Callable(const Function &function)
+    : m_callable(function) {
+}
+
+std::any Callable::call(const Interpreter &interpreter, const std::vector<Literal> &arguments) const {
+  return std::visit(overload{
+                        [&](const std::monostate) { return std::any{}; },                               //
+                        [&](const Class &klass) { return klass.call(interpreter, arguments); },         //
+                        [&](const Function &function) { return function.call(interpreter, arguments); } //
+                    },
+                    m_callable);
+}
+
+std::size_t Callable::arity() const {
+  return std::visit(overload{
+                        [&](const std::monostate) { return std::size_t{0}; },      //
+                        [&](const Class &klass) { return klass.arity(); },         //
+                        [&](const Function &function) { return function.arity(); } //
+                    },
+                    m_callable);
+}
+
+Callable::operator std::string() const {
+  return std::visit(overload{
+                        [&](const std::monostate) { return std::string{}; },            //
+                        [&](const Class &klass) { return std::string(klass); },         //
+                        [&](const Function &function) { return std::string(function); } //
+                    },
+                    m_callable);
+}
+
+}
