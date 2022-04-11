33c2a063edf1072d7815122d31572f8b8608923c
vanilla milkshake <vanillamilkshakepls@gmail.com>
2022-04-11 16:17:21 +0300

    Fix: interpreting set expressions

--- src/interpreter/interpreter.cpp
+++ src/interpreter/interpreter.cpp
@@ -49,11 +49,11 @@ Literal Interpreter::ExpressionVisitor::operator()(const LogicalExpr &expr) cons
 std::any Interpreter::ExpressionVisitor::operator()(const SetExpr &expr) const {
   std::any object = evaluate(expr);
 
-  if (object.type() == typeid(Instance))
+  if (object.type() != typeid(Instance))
     throw RuntimeError(expr.name, "Only instances have fields.");
 
   std::any value = evaluate(expr.value);
-  std::any_cast<Instance>(value).set(expr.name, value);
+  std::any_cast<Instance>(object).set(expr.name, value);
   return value;
 }
 
